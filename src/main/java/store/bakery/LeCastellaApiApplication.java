fccf254d2032826f1fd09043346a2b17c21c8e7c
ZONE17TH\Zone17th <minhtruong53262@gmail.com>
2023-11-12 20:59:16 +0700

    base

---
+++ src/main/java/store/bakery/LeCastellaApiApplication.java
@@ -0,0 +1,13 @@
+package store.bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeCastellaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeCastellaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/bakery/LeCastellaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeCastellaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
